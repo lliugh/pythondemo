418410700ebc1da72b1b0c549a565f80d140a21e
lliugh <guohaoliu@gmail.com>
2016-02-01 00:04:57 +0800

    first commit

---
+++ src/test1.py
@@ -0,0 +1,9 @@
+"""打印Python版本号"""
+__author__ = 'lliugh'
+from platform import python_version
+import sys
+
+print("Python "+ python_version())
+print("Python "+ sys.version)
+print(sys.version_info)
+print("hello world")
---
+++ src/test2.py
@@ -0,0 +1,13 @@
+"""操作列表"""
+__author__ = 'lliugh'
+list = ["first","second","third"];
+print(list);
+print(list[2]);
+print(len(list));
+
+count = 0;
+while(count < len(list)):
+    print(list[count]);
+    count = count + 1
+
+
